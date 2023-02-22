d379799ecc746cb24acdef0c2f44413b476a7c7d
dankieu <kieudinhdan02@gmail.com>
2023-02-22 08:41:58 +0700

    modal

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
-import styles from './modal.module.scss';
-Modal.setAppElement('#root');
+
+
+import classNames from 'classnames/bind';
+import styles from './modal.module.scss'
+const cx = classNames.bind(styles);
+
 
 function ModalMy() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleEditClick =()=>{
+    setIsModalOpen(!isModalOpen);
+  }
 
   function handleEditClick() {
     setIsModalOpen(true);
@@ -16,27 +22,40 @@ function ModalMy() {
 
   return (
     <div>
-      <button onClick={handleEditClick}>Chỉnh sửa</button>
-      <Modal  isOpen={isModalOpen} onRequestClose={handleCloseModal}>
-        <h1>Sửa Hồ Sơ</h1>
-        <hr style={{ color: '#222' }} />
+      <button onClick={handleEditClick}  >Sửa hồ sơ</button>
+      {modal &&C
+       <div className={cx("modal")}>
+        <div onClick={handleEditClick } className={cx("overlay")}>
 
-        <form>
-          {/* Form để chỉnh sửa thông tin */}
+        </div>
+        <div className={cx("modal-content")}>
+           <h1>Sửa hồ sơ</h1>
+           <hr />
+           <div>
+              <img src="" alt="" />
+           </div>
+        <div>
+          
           <label for="name">Name:</label>
-          <input type="text" id="name" name="name" placeholder="Your name" />
+          <input type="text" id="name" name="name" placeholder="Your name"/>
 
           <label for="username">Username:</label>
-          <input type="text" id="username" name="username" placeholder="Your username" />
+          <input type="text" id="username" name="username" placeholder="Your username"/>
 
           <label for="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder="Your email" />
+          <input type="email" id="email" name="email" placeholder="Your email"/>
 
           <label for="bio">Bio:</label>
           <textarea id="bio" name="bio" placeholder="Your bio"></textarea>
-          <button onClick={handleCloseModal}>Đóng</button>
-        </form>
-      </Modal>
+        
+         <button type="submit">Save</button>
+       </div>
+
+
+        </div>
+       </div>
+      }
+      
     </div>
   );
 }
