f5dc5570383356599972199345894b012fa25e6c
bachquanghung <115093833+bachquanghung@users.noreply.github.com>
2023-02-26 20:54:12 +0700

    m

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Login.module.scss';
 import classNames from 'classnames/bind';
 import { AiOutlineScan } from 'react-icons/ai';
@@ -9,54 +9,81 @@ import { RiEyeCloseLine } from 'react-icons/ri';
 import { RiEyeFill } from 'react-icons/ri';
 const cx = classNames.bind(styles);
 const Login = () => {
+
   const [userValid, setUserValid] = useState(true);
   const [passValid, setPassValid] = useState(true);
-  const [account, setAccount]=useState({user:'',pass:''});
+  const [account, setAccount] = useState({ user: '', pass: '' });
   const [hidden, setHidden] = useState(true);
+  const [showSuggest, setShowSuggest] = useState(false);
 
-  const passw= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+  const [searchResult, setSearchResult] = useState([]);
+  const [suggestion, setSuggestion] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://63fa02d9897af748dcc7907c.mockapi.io/account`)
+      .then((res) => res.json())
+      .then((res) => {
+        setSearchResult(res);
+      });
+  }, []);
+
+  const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 
   const handleUserChange = (event) => {
     const value = event.target.value;
-    if(value.length>=8||value==''){
-        setUserValid(true)
+    if (value.length >= 8 || value == '') {
+      setUserValid(true);
+    } else {
+      setUserValid(false);
     }
-    else{
-        setUserValid(false)
+    let matches = [];
+    if (value.length > 0) {
+      matches = searchResult.filter((usr) => {
+        const regex = new RegExp(`${value}`, 'gi');
+        return usr.username.match(regex);
+      });
     }
-
-    const myTimeOut=setTimeout(()=>{setAccount(
-        {
+   
+      setAccount({
         ...account,
         user: value,
-      }
-      )},4000)
-  }
+      });
+   
+    setSuggestion(matches);
+    setShowSuggest(true);
+  };
 
-    const handlePassChange = (event) => {
-        const value = event.target.value; 
-     
-        if((value.match(passw)&&value.length>=8)||value==''){
-            setPassValid(true)
-        }
-        else{
-            setPassValid(false)
-        }   
-      const myTimeOut=setTimeout(()=>{setAccount(
-        {
+  const hideSuggest = () => {
+    setShowSuggest(false);
+  };
+
+  const chooseSuggest = (text) => {
+    setAccount({
+      ...account,
+      user: text,
+    })
+  };
+
+  const handlePassChange = (event) => {
+    const value = event.target.value;
+
+    if ((value.match(passw) && value.length >= 8) || value == '') {
+      setPassValid(true);
+    } else {
+      setPassValid(false);
+    }
+      setAccount({
         ...account,
         pass: value,
-      }
-      )},4000)
-       
-  };
+      });
+  }
 
   function showPass() {
     setHidden((prev) => !prev);
   }
 
   return (
-    <div className={cx('container')}>
+    <div className={cx('container')} onClick={hideSuggest}>
       <div className={cx('title')}>Log in</div>
       <div className={cx('loginField')}>
         <div className={cx('userNameChoice')}>
@@ -64,21 +91,30 @@ const Login = () => {
           <div>Log in with phone</div>
         </div>
         <div className={cx('loginDetail')}>
-          <div className={userValid?cx('userName'):cx('userNameFalse')}>
-            <input type="text" style={{ width: '80%' }} onChange={handleUserChange}></input>
+          <div className={userValid ? cx('userName') : cx('userNameFalse')}>
+            <input type="text" style={{ width: '80%' }} onChange={handleUserChange} value={account.user}></input>
           </div>
-          <div style={{color:'red'}}>{userValid?'':'Your username is invalid!'}</div>
-          <div className={passValid?cx('passWord'):cx('passWordFalse')}>
-            <input type={hidden?'password':'text'} style={{ width: '80%' }} onChange={handlePassChange}></input>
+          <div className={cx('suggestions')}>
+            {showSuggest
+              ? suggestion.map((suggest, i) => (
+                  <div key={i} className={cx('suggestion')} onClick={chooseSuggest(suggest.username)}>
+                    {suggest.username}
+                  </div>
+                ))
+              : ''}
+          </div>
+          <div style={{ color: 'red' }}>{userValid ? '' : 'Your username is invalid!'}</div>
+          <div className={passValid ? cx('passWord') : cx('passWordFalse')}>
+            <input type={hidden ? 'password' : 'text'} style={{ width: '80%' }} onChange={handlePassChange}></input>
             {hidden ? (
-                <span onClick={showPass}>
-              <RiEyeCloseLine style={{ cursor: 'pointer' }}></RiEyeCloseLine>
+              <span onClick={showPass}>
+                <RiEyeCloseLine style={{ cursor: 'pointer' }}></RiEyeCloseLine>
               </span>
             ) : (
               <RiEyeFill style={{ cursor: 'pointer' }} onClick={showPass}></RiEyeFill>
             )}
           </div>
-          <div style={{color:'red'}}>{passValid?'':'Your password is invalid!'}</div>
+          <div style={{ color: 'red' }}>{passValid ? '' : 'Your password is invalid!'}</div>
         </div>
         <div className={cx('forgotPass')}>Forgot passWord?</div>
         <button className={cx('loginButton')} type="submit">
--- src/pages/UpLoad/Upload.js
+++ src/pages/UpLoad/Upload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './Upload.module.scss';
 import classNames from 'classnames/bind';
 import Footer from './Footer';
@@ -7,16 +7,24 @@ import Toggle from './Toggle';
 
 const cx = classNames.bind(styles);
 const UpLoad = () => {
-  const[charNumber,setCharNumber]=useState(0)
+  const [charNumber, setCharNumber] = useState(0);
 
-  const handleCharNumber=(event)=>{
-    const value=event.target.value;
-    const num=value.length;
-    setCharNumber(num);
+  const handleRecivedFile = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      console.log(reader.result)
+    }
   }
+
+  const handleCharNumber = (event) => {
+    const value = event.target.value;
+    const num = value.length;
+    setCharNumber(num);
+  };
   return (
     <div>
-   
       <div className={cx('uploadContainer')}>
         <div className={cx('title')}>
           <div className={cx('bigTitle')}>Tải video lên</div>
@@ -39,14 +47,16 @@ const UpLoad = () => {
                 <div className={cx('uploadContent')}>Nhỏ hơn 2 GB</div>
               </div>
               <button className={cx('uploadBtn')}>
-                <input type="file" id="upload" hidden />
-                <label for="upload">Chọn tập tin</label>
+                <input onChange={handleRecivedFile} type="file" id="upload" hidden />
+                <label htmlFor="upload">Chọn tập tin</label>
               </button>
             </div>
           </div>
           <div className={cx('adjustField')}>
             <div className={cx('fixField')}>
-              <div className={cx('fixIcon')}><img src='https://lf16-tiktok-common.ttwstatic.com/obj/tiktok-web-common-sg/ies/creator_center/svgs/divide_black.e1e40d5b.svg'></img></div>
+              <div className={cx('fixIcon')}>
+                <img src="https://lf16-tiktok-common.ttwstatic.com/obj/tiktok-web-common-sg/ies/creator_center/svgs/divide_black.e1e40d5b.svg"></img>
+              </div>
               <div className={cx('fixContent')}>
                 <div className={cx('fieldTile')}>Chia video và chỉnh sửa</div>
                 <div className={cx('fieldContent')}>
@@ -56,75 +66,71 @@ const UpLoad = () => {
               </div>
               <button className={cx('btn')}>Chỉnh sửa</button>
             </div>
-          
 
-          <div className={cx('noteField')}>
-            <div className={cx('noteContent')}>
-              <div className={cx('fieldTile')}>Chú thích</div>
-              <div className={cx('numberOfChar')}>{charNumber}/150</div>
-            </div>
-            <div className={cx('inputNote')}>
-              <input type='text' style={{width:'100%'}} onChange={handleCharNumber}>
-              </input>
-              <div>
-              <span className={cx('inputNoteChoice')}>@</span>
-              <span className={cx('inputNoteChoice')}>#</span>
+            <div className={cx('noteField')}>
+              <div className={cx('noteContent')}>
+                <div className={cx('fieldTile')}>Chú thích</div>
+                <div className={cx('numberOfChar')}>{charNumber}/150</div>
+              </div>
+              <div className={cx('inputNote')}>
+                <input type="text" style={{ width: '100%' }} onChange={handleCharNumber}></input>
+                <div>
+                  <span className={cx('inputNoteChoice')}>@</span>
+                  <span className={cx('inputNoteChoice')}>#</span>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className={cx('coverField')}>
-            <div className={cx('fieldTile')}>Ảnh bìa</div>
-            <div className={cx('cover')}></div>
-          </div>
+            <div className={cx('coverField')}>
+              <div className={cx('fieldTile')}>Ảnh bìa</div>
+              <div className={cx('cover')}></div>
+            </div>
 
-          <div className={cx('restrictField')}>
-            <div className={cx('fieldTile')}>Ai có thể xem video này</div>
-            <select className={cx('restrictOption')}>
-              <option className={cx('Option')}>Công khai</option>
-              <option className={cx('Option')}>Bạn bè</option>
-              <option className={cx('Option')}>Riêng tư</option>
-            </select>
-          </div>
+            <div className={cx('restrictField')}>
+              <div className={cx('fieldTile')}>Ai có thể xem video này</div>
+              <select className={cx('restrictOption')}>
+                <option className={cx('Option')}>Công khai</option>
+                <option className={cx('Option')}>Bạn bè</option>
+                <option className={cx('Option')}>Riêng tư</option>
+              </select>
+            </div>
 
-          <div className={cx('allow')}>
-            <div className={cx('fieldTile')}>Cho phép người dùng:</div>
-            <div className={cx('allowChoices')}>
-            <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
-            <label for="vehicle1">Bình luận</label>
+            <div className={cx('allow')}>
+              <div className={cx('fieldTile')}>Cho phép người dùng:</div>
+              <div className={cx('allowChoices')}>
+                <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
+                <label htmlFor="vehicle1">Bình luận</label>
 
-            <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
-            <label for="vehicle1">Duet</label>
+                <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
+                <label htmlFor="vehicle1">Duet</label>
 
-            <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
-            <label for="vehicle1">Stitch</label>
+                <input type="checkbox" name="vehicle1" className={cx('checkbox1')}></input>
+                <label htmlFor="vehicle1">Stitch</label>
+              </div>
+            </div>
+
+            <div className={cx('author')}>
+              <div className={cx('authorHead')}>
+                <div className={cx('fieldTile')}>Chạy quy trình kiểm tra bản quyền</div>
+                <Toggle></Toggle>
+              </div>
+              <div className={cx('fieldContent')}>
+                Chúng tôi sẽ kiểm tra xem video của bạn có sử dụng âm thanh vi phạm bản quyền hay không. Nếu chúng tôi
+                phát hiện có vi phạm, bạn có thể chỉnh sửa video trước khi đăng
+              </div>
+            </div>
+
+            <div className={cx('buttons')}>
+              <button className={cx('cancelBtn')}>Hủy Bỏ</button>
+              <button className={cx('btn')}>Đăng</button>
             </div>
           </div>
-
-          <div className={cx('author')}>
-            <div className={cx('authorHead')}>
-              <div className={cx('fieldTile')}>Chạy quy trình kiểm tra bản quyền</div>
-              <Toggle></Toggle>
-            </div>
-            <div className={cx('fieldContent')}>
-              Chúng tôi sẽ kiểm tra xem video của bạn có sử dụng âm thanh vi phạm bản quyền hay không. Nếu chúng tôi
-              phát hiện có vi phạm, bạn có thể chỉnh sửa video trước khi đăng
-            </div>
-          </div>
-
-<div className={cx('buttons')}>
-<button className={cx('cancelBtn')}>Hủy Bỏ</button>
-<button className={cx('btn')}>Đăng</button>
-</div>
-</div>
         </div>
       </div>
-     
 
-     <Login></Login>
+      <Login></Login>
       <Footer></Footer>
     </div>
-
   );
 };
 
