9f77e4b5a193e7bbbbca42f6f9f02babb2c07e3e
chuyenpro <115093851+chuyenpro@users.noreply.github.com>
2023-02-28 20:53:21 +0700

    m

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,43 +1,41 @@
 import { useState, useEffect } from 'react';
 import ReelsHome from 'components/ReelsHome/ReelsHome';
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
 import classNames from 'classnames/bind';
 
-const cx=classNames.bind(styles)
+const cx = classNames.bind(styles);
 const Home = () => {
-  const [user, setUser] = useState([]);
+  const [objects, setObjects] = useState([]);
   const [showUser, setshowUser] = useState(false);
   const [randomReels, setRandomReels] = useState();
   useEffect(() => {
     fetch('https://63de107ff1af41051b0d0b2c.mockapi.io/videotiktok')
-    .then(res=>res.json())
-    .then((res)=>{
-      setUser(res)
-      console.log(res)
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        setObjects(data);
+        console.log(data);
+      });
   }, []);
-  
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY) {
-        setshowUser(true);
-      } else {
-        setshowUser(false);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY) {
+  //       setshowUser(true);
+  //     } else {
+  //       setshowUser(false);
+  //     }
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
 
-    // Cleanup function
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   // Cleanup function
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
   return (
     <div className={cx('wrapper')}>
-      {user.map(use=>(
-
-      <ReelsHome key={use.id} data={use}/>
+      {objects.map(object=>(
+        <ReelsHome key={object.id} data={object} />
       ))}
     </div>
   );
